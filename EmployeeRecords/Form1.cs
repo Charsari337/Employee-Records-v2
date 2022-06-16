c9c461d1290cf2393b1329caf594a1031bf48dd9
Charles Sarichith <charsari337@ed.amdsb.ca>
2022-06-16 08:51:42 -0400

    for charles

--- EmployeeRecords/Form1.cs
+++ EmployeeRecords/Form1.cs
@@ -17,10 +17,22 @@ namespace EmployeeRecords
         public mainForm()
         {
             InitializeComponent();
+            loadDB();
         }
 
         private void addButton_Click(object sender, EventArgs e)
         { 
+            //Get all your inputs
+            string id = idInput.Text;
+            string firstName = fnInput.Text;
+            string lastName = lnInput.Text;
+            string date = dateInput.Text;
+            string salary = salaryInput.Text;
+
+            //create an employee object with the input values
+            Employee employee = new Employee(id, firstName, lastName, date, salary);
+            employeeDB.Add(employee);
+
             ClearLabels();
         }
 
@@ -31,7 +43,12 @@ namespace EmployeeRecords
 
         private void listButton_Click(object sender, EventArgs e)
         {
+            outputLabel.Text = "";
 
+            foreach(Employee emp in employeeDB)
+            {
+                outputLabel.Text += $"{emp.id} {emp.firstName} {emp.lastName} {emp.date} {emp.salary} \n";
+            }
         }
 
         private void ClearLabels()
@@ -45,17 +62,60 @@ namespace EmployeeRecords
 
         private void mainForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-
+            saveDB();
         }
 
         public void loadDB()
         {
-            
+            XmlReader reader = XmlReader.Create("Resources/employeeData.xml");
+
+            while (reader.Read())
+            {
+                if(reader.NodeType == XmlNodeType.Text)
+                {
+                    string id = reader.ReadString();
+
+                    reader.ReadToNextSibling("firstname");
+                    string firstName = reader.ReadString();
+
+                    reader.ReadToNextSibling("lastname");
+                    string lastName = reader.ReadString();
+
+                    reader.ReadToNextSibling("date");
+                    string date = reader.ReadString();
+
+                    reader.ReadToNextSibling("pay");
+                    string salary = reader.ReadString();
+
+                    Employee employee = new Employee(id, firstName, lastName, date, salary);
+                    employeeDB.Add(employee);
+                }
+            }
+
+            reader.Close();
         }
 
         public void saveDB()
         {
+            XmlWriter writer = XmlWriter.Create("Resources/employeeData.xml", null);
 
+            writer.WriteStartElement("Employees");
+
+            foreach(Employee emp in employeeDB)
+            {
+                writer.WriteStartElement("Employee");
+
+                writer.WriteElementString("id", emp.id);
+                writer.WriteElementString("firstname", emp.firstName);
+                writer.WriteElementString("lastname", emp.lastName);
+                writer.WriteElementString("date", emp.date);
+                writer.WriteElementString("pay", emp.salary);
+
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+            writer.Close();
         }
     }
 }
